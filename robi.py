7a03ee7c84bbd88a1cef98d0e6c0ae856a77fdb3
harizMunawar <munawarhariz@gmail.com>
2020-07-02 17:53:43 +0700

    Added Simple Command (Ping and Get User)

--- robi.py
+++ robi.py
@@ -1,15 +1,18 @@
-import discord
+from discord.ext import commands
 
-client = discord.Client()
+bot = commands.Bot('!') #prefix
 
-@client.event
+@bot.event
 async def on_ready():
-  print(f'{client.user} has connected to Discord!')
+  print(f'{bot.user} has connected to Discord!')
     
-@client.event
-async def on_message(message): 
-    if message.content.find("!sevi") != -1:
-        await message.channel.send('halo om aku bot sevi')
-    elif message.content.find("!tugas") != -1:
-        await message.channel.send('libur tolollll')
-client.run("NzI3ODY2MDUxNTU5MTYxOTM2.XvyHYg.vxxKbFe1QnCniBhNMkODispYla0")
+@bot.command(aliases = ['who', 'saha']) #alias untuk command (pake prefix)
+async def siapa(ctx): #nama command (harus dipanggil pakai prefix diatas)
+  message = ctx.channel
+  await message.send("kamu adalah {}".format(ctx.author.nick))
+
+@bot.command()
+async def ping(ctx):
+  await ctx.channel.send(f'{round(bot.latency*1000)}ms')
+
+bot.run("NzI3ODY2MDUxNTU5MTYxOTM2.XvyHYg.vxxKbFe1QnCniBhNMkODispYla0")
