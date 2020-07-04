5c7255513ddc79258178be7a1bec6185b200a4c7
harizMunawar <munawarhariz@gmail.com>
2020-07-04 12:58:11 +0700

    Connection With Imgur API

--- robi.py
+++ robi.py
@@ -1,42 +1,20 @@
 from discord.ext import commands
 import random
+from imgurpython import ImgurClient #python imgur api wrapper
 
 bot = commands.Bot('!') #prefix
+
+client = ImgurClient('4db5ce905276402', '7d5187662ae124628c44e4cfdb368a2e13e86167')
 
 @bot.event
 async def on_ready():
   print(f'{bot.user} has connected to Discord!')
-    
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandOnCooldown):
-      message = ctx.channel
-      await message.send('Cooldown boss, coba lagi setelah {:.2f}s'.format(error.retry_after))
-    raise error
 
-@bot.command(aliases = ['who', 'saha']) #alias untuk command (pake prefix)
-async def siapa(ctx): #nama command (harus dipanggil pakai prefix diatas)
-  message = ctx.channel
-  await message.send("kamu adalah {}".format(ctx.author.nick))
+@bot.command(aliases = ['image', 'imgur', 'pict']) 
+async def gambar(ctx, search_query = ""):
+  images = client.gallery_search(search_query, advanced=None, sort='time', window='all', page=0)
+  random_result = random.choice(images)
+  image_title = random_result.title if random_result.title else 'Untitled'
+  await ctx.channel.send('{0}\n{1}'.format(image_title, random_result.link))
 
-@bot.command()
-async def ping(ctx):
-  await ctx.channel.send(f'{round(bot.latency*1000)}ms')
-
-@bot.command()
-@commands.cooldown(1, 5)
-async def code(ctx):
-  i = 0
-  kode = ''
-  while i < 6:
-    if i == 0:
-      kode += str(random.randint(1,2))
-    else:
-      kode += str(random.randint(0,9))
-    i += 1
-
-  message = ctx.channel
-  await message.send(kode)
-
-
-bot.run("NzI3ODY2MDUxNTU5MTYxOTM2.XvyHYg.vxxKbFe1QnCniBhNMkODispYla0")
+bot.run("NzI3ODY2MDUxNTU5MTYxOTM2.XwAXCQ.TwvoeGhGG0o8CRV_BNX5zfE7qH0")
