f7a9b21cf3e55b2c6096c1b0d1d8ecf39d737892
harizMunawar <munawarhariz@gmail.com>
2020-07-07 15:11:37 +0700

    Randoming search result

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from discord import Embed
+import random
 import praw
 from prawcore import exceptions
 from robiconf.bot_configuration import RedditClient
@@ -17,22 +18,18 @@ class Reddit(commands.Cog):
             zero_result = True
             searchword = ""                        
             for n in args:          
-                searchword = searchword + " " + n
-            posts = reddit_client.subreddit(subreddit_search).search(submission_search+" "+searchword, limit=1)
+                searchword = searchword + " " + n        
+            posts = random.choice([x for x in reddit_client.subreddit(subreddit_search).search(submission_search+" "+searchword, limit=20)])            
         else:
-            posts = reddit_client.subreddit(subreddit_search).random_rising(limit = 1)
-    
-        for submission in posts:    
-            zero_result = False
-            if submission.thumbnail:
-                embedVar = Embed(title=submission.title, url=submission.url)
-                embedVar.set_image(url=submission.url)
-            else:
-                embedVar = Embed(title=submission.title, url=submission.url)
-            await ctx.channel.send(embed = embedVar)
-            if not submission.thumbnail and not submission.selftext == '': await ctx.channel.send(f' >>> {deskripsi}')
-        if zero_result:
-            await ctx.channel.send('Submission Gagal Ditemukan atau Tidak Tersedia')
+            posts = random.choice([x for x in reddit_client.subreddit(subreddit_search).random_rising(limit = 1)])
+       
+        if posts.thumbnail:
+            embedVar = Embed(title=posts.title, url=posts.url)
+            embedVar.set_image(url=posts.url)
+        else:
+            embedVar = Embed(title=posts.title, url=posts.url)
+        await ctx.channel.send(embed = embedVar)
+        if not posts.thumbnail and not posts.selftext == '': await ctx.channel.send(f' >>> {posts.selftext}')
             
 
     @reddit.error
@@ -42,6 +39,9 @@ class Reddit(commands.Cog):
 
         if isinstance(error, exceptions.Redirect):
             await ctx.channel.send('Subreddit Gagal Ditemukan atau Tidak Tersedia')
+
+        if isinstance(error, CommandInvokeError):
+            await ctx.channel.send('Submission Gagal Ditemukan atau Tidak Tersedia')
 
         raise error
 
