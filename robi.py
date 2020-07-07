393448698536e987972b4f68a4ad5c712b69e550
harizMunawar <munawarhariz@gmail.com>
2020-07-07 14:38:22 +0700

    Removing annoying response message when loading or unloading Cogs

--- robi.py
+++ robi.py
@@ -9,11 +9,18 @@ from robiconf.bot_configuration import Token, BotInstance
 
 bot = BotInstance.bot #ngambil object bot dari robiconf
 
+@bot.command()
+async def load(ctx, extension):
+  bot.load_extension(f'cogs.{extension}')
+
+@bot.command()
+async def unload(ctx, extension):
+  bot.unload_extension(f'cogs.{extension}')
+
 @bot.command(aliases=['re'])
 async def refresh(ctx, extension):
   bot.unload_extension(f'cogs.{extension}')
   bot.load_extension(f'cogs.{extension}')
-  await ctx.channel.send(f'Cogs {extension} Berhasil di Refresh')
 
 @refresh.error
 async def load_error(ctx, error):
@@ -21,7 +28,7 @@ async def load_error(ctx, error):
       bot.load_extension(f'cogs.{extension}')
       await ctx.channel.send(f'Cogs {extension} Berhasil di Refresh')
   if isinstance(error, commands.MissingRequiredArgument):
-    await ctx.channel.send('Kesalahan Penggunan Command.\n > !load <namacogs>')
+    await ctx.channel.send('Kesalahan Penggunan Command.\n > !re <namacogs>')
   raise error
 
 for filename in os.listdir('./cogs'):
@@ -31,5 +38,11 @@ for filename in os.listdir('./cogs'):
 @bot.event
 async def on_ready():
   print(f'{bot.user} has connected to Discord!')
+
+@bot.event   
+async def on_message(message):
+  if "!re" in str(message.content) or "!load" in str(message.content) or "!unload" in str(message.content):
+    await message.add_reaction('👍')
+  await bot.process_commands(message)
 
 bot.run(Token.token)
