608d09221b59bf3b87df96536c70b95c0ba328b9
Hariz Sufyan Munawar <munawarhariz@gmail.com>
2021-04-28 13:47:59 +0700

    Integration with go blackjack api

--- cogs/card.py
+++ cogs/card.py
@@ -5,7 +5,8 @@ from discord.ext.commands import Context
 from configuration import BotInstance
 import requests
 
-deckAPI = "https://go-deck.harizmunawar.repl.co/deck"
+# deckAPI = "https://go-deck.harizmunawar.repl.co/deck"
+deckAPI = "http://localhost:8000"
 currAPI = "https://robo-hasbi-currency-api.ganiyamustafa.repl.co"
 bot = BotInstance.bot
 
@@ -25,24 +26,27 @@ class CardGame(commands.Cog):
             await ctx.send(embed=Embed().add_field(name="Tidak Dapat Memulai Game Blackjack", value="Anda Tidak Punya Uang Sebanyak Itu Untuk Digunakan Berjudi"))
             return
         
-        player_hand = []
-        dealer_hand = []
-        deck_id, player_hand, dealer_hand = init_bj(player_hand, dealer_hand)
-        
-        initGame = Embed(color=0xecce8b)
-        initGame.set_author(name=f"{player.name} Memulai Blackjack", icon_url=player.avatar_url)
-        initGame.add_field(name=player.name, value=f"Cards - {' '.join(card_parser(player_hand))}\nScore - {score_counter(player_hand)}")
-        initGame.add_field(name="Bang Hasbi", value=f"Cards - {card_parser(dealer_hand)[0]} ? \nScore - {score_counter(dealer_hand, 1)}")
-        initGame.set_footer(text='Ketik h untuk hit, s untuk stand, atau a untuk akhiri permainan')
 
-        await ctx.send(embed=initGame)
+        # Create new blackjack game
+        game = requests.get(deckAPI+"/blackjack/create")
+        if not game.status_code == 201:
+            await ctx.send(embed=Embed().add_field(name="Tidak Dapat Memulai Game Blackjack", value="Terjadi Kesalahan Saat Mencoba Membuat Game"))
+            return
+
+        game = game.json()["blackjack"]
+        game_id = game["id"]
+        player_hand = [card for card in game["playerHand"]]
+        dealer_hand = [card for card in game["dealerHand"]]
+        winner = game["winner"]
+        finished = game["finished"]
+    
+        await ctx.send(embed=bj_embed(player, player_hand, dealer_hand, winner, bet, finished))
 
         try:
-            finished = None
             while not finished:
-                finished, game_embed = game_finished(player, player_hand, dealer_hand, bet)
+                status = requests.get(deckAPI+"/blackjack/"+str(game_id)+"/status").json()["blackjack"]
 
-                if not finished:
+                if not status["finished"]:
                     reply = await bot.wait_for('message', check=lambda m: (m.content.lower() in ['h', 's', 'a'] or m.content.lower().startswith('!bj')) and m.author == player, timeout=120)
                     message = reply.content.lower()
 
@@ -50,50 +54,43 @@ class CardGame(commands.Cog):
                         await ctx.send("Anda masih memiliki game yang sedang berlangsung.")
 
                     if message == 'h':
-                        player_hand = hit(deck_id, player_hand)
-                        finished, game_embed = game_finished(player, player_hand, dealer_hand, bet)
-
-                    if message in ['s', 'h']:
-                        dealer_hand = dealer_ai(deck_id, dealer_hand)
-                        finished, game_embed = game_finished(player, player_hand, dealer_hand, bet)
+                        requests.get(deckAPI+"/blackjack/"+str(game_id)+"/hit")
+                        requests.get(deckAPI+"/blackjack/"+str(game_id)+"/dealer-ai")
+                        status = requests.get(deckAPI+"/blackjack/"+str(game_id)+"/status").json()["blackjack"]
 
                     if message == 's':
-                        finished, game_embed = game_finished(player, player_hand, dealer_hand, bet, True)
+                        requests.get(deckAPI+"/blackjack/"+str(game_id)+"/dealer-ai?stand=true")
+                        status = requests.get(deckAPI+"/blackjack/"+str(game_id)+"/status").json()["blackjack"]
 
                     if message == 'a':
                         finished = True
                         game_embed = Embed(color=0xff0000).add_field(name="Permainan Dihentikan", value=f"{player.name} Harus Membayar {bet/2}N$")
                         requests.post(currAPI+"/curr/add/"+str(player.id)+"/"+str((bet/2)))
 
+                player_hand = [card for card in status["playerHand"]]
+                dealer_hand = [card for card in status["dealerHand"]]
+                winner = status["winner"]
+                message = status["message"]
+                finished = status["finished"]
+                
+                if not message == 'a': game_embed = bj_embed(player, player_hand, dealer_hand, winner, bet, finished, message)
+
                 await ctx.send(embed=game_embed)
 
-            winner, message = game_state(player, player_hand, dealer_hand)
-            if winner == player.name:
+            if winner == "Player":
                 requests.post(currAPI+"/curr/add/"+str(player.id)+"/"+str(bet))
-            elif winner == 'Bang Hasbi':
+            elif winner == 'Dealer':
                 requests.post(currAPI+"/curr/min/"+str(player.id)+"/"+str(bet))
 
         except asyncio.TimeoutError:
             await ctx.send(f"{player.name} tidak menjawab dalam 2 menit :(")
 
-        requests.delete(deckAPI+"/"+str(deck_id))
-
-def game_finished(player, player_hand, dealer_hand, bet, force_finished=False):
-    winner, message = game_state(player, player_hand, dealer_hand)
-    player_score = score_counter(player_hand)
-    dealer_score = score_counter(dealer_hand)
-
-    if dealer_score == 21 or player_score == 21 or player_score >= 21 or (dealer_score >= 21 and force_finished) or force_finished:
-        game_embed = bj_embed(player, player_hand, dealer_hand, winner, bet, True, message)
-        return True, game_embed
-    
-    game_embed = bj_embed(player, player_hand, dealer_hand, winner, bet, False, message)
-    return False, game_embed
+        requests.delete(deckAPI+"/deck/"+str(game["deckId"]))
 
 def bj_embed(player, player_hand, dealer_hand, winner, bet, finished, message=""):
-    color = 0x00ff00 if winner == player.name else 0xff0000
+    color = 0x00ff00 if winner == "Player" else 0xff0000
     color = color if finished else 0xecce8b
-    winner_name = player.name if winner == player.name else 'Bang Hasbi'
+    winner_name = player.name if winner == "Player" else 'Bang Hasbi'
 
     game_embed = Embed(color=color)
     game_embed.set_author(name=f"{player.name} Memulai Blackjack", icon_url=player.avatar_url)
@@ -102,7 +99,7 @@ def bj_embed(player, player_hand, dealer_hand, winner, bet, finished, message=""
     if finished and winner == 'Draw':
         game_embed.add_field(name=f"Skor Seri!!! {message}", value="Tidak Ada Yang Harus Kehilangan Uang", inline=False)
     elif finished:
-        finished_value = f"{player.name} Mendapat {bet}N$" if winner == player.name else f"{player.name} Harus Membayar {bet}N$"
+        finished_value = f"{player.name} Mendapat {bet}N$" if winner == "Player" else f"{player.name} Harus Membayar {bet}N$"
         game_embed.add_field(name=f"{winner_name} Menang!!! {message}", value=finished_value, inline=False)
 
     game_embed.add_field(name=player.name, value=f"Cards - {' '.join(card_parser(player_hand))}\nScore - {score_counter(player_hand)}")
@@ -112,31 +109,6 @@ def bj_embed(player, player_hand, dealer_hand, winner, bet, finished, message=""
         return game_embed
     
     return game_embed.add_field(name="Bang Hasbi", value=f"Cards - {card_parser(dealer_hand)[0]} ? \nScore - {score_counter(dealer_hand, 1)}")
-
-def init_bj(player_hand, dealer_hand):
-    deck = requests.get(deckAPI+"/create?shuffle=true")
-
-    if deck.status_code == 201:
-        deckId = deck.json()['deckId']
-
-        for i in range (0,2):
-            draw = requests.get(deckAPI+"/"+str(deckId)+"/draw")
-            if draw.status_code == 200:
-                player_hand.append(draw.json()['card'])
-
-        for i in range (0,2):
-            draw = requests.get(deckAPI+"/"+str(deckId)+"/draw")
-            if draw.status_code == 200:
-                dealer_hand.append(draw.json()['card'])
-
-    return deckId, player_hand, dealer_hand
-
-def hit(deck_id, hand):
-    draw = requests.get(deckAPI+"/"+str(deck_id)+"/draw")
-    if draw.status_code == 200:
-        hand.append(draw.json()['card'])
-
-    return hand
 
 def card_parser(list_card):
     suit_symbols = ['♠', '♦', '♣', '♥']
@@ -161,28 +133,6 @@ def score_counter(list_card, n=None, with_rank=False):
     if with_rank: return score, rank_list
 
     return score
-
-def game_state(player, player_hand, dealer_hand):
-    player_score = score_counter(player_hand)
-    dealer_score = score_counter(dealer_hand)
-    if player_score > 21: return 'Bang Hasbi', 'Anda Busted'
-    if dealer_score > 21: return player.name, 'Bang Hasbi Busted'
-    if dealer_score == 21: return 'Bang Hasbi', 'Bang Hasbi Mendapat Blackjack'
-    if player_score == 21: return player.name, 'Anda Mendapat Blackjack'
-    if player_score > dealer_score and player_score < 21: return player.name, 'Skor Anda Lebih Besar Dari Bang Hasbi'
-    if dealer_score > player_score and dealer_score < 21: return 'Dealer', 'Skor Bang Hasbi Lebih Besar Dari Anda'
-    if dealer_score == player_score: return 'Draw', 'Skor Kalian Sama Ciee'
-
-    return None, None
-
-def dealer_ai(deck_id, dealer_hand):
-    dealer_score, rank_list = score_counter(dealer_hand, with_rank=True)
-
-    while dealer_score <= 16 or (dealer_score == 17 and 1 in rank_list):
-        dealer_hand = hit(deck_id, dealer_hand)
-        dealer_score = score_counter(dealer_hand)
-
-    return dealer_hand
 
 
 def setup(bot):
