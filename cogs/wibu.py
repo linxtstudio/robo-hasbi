505d9d99bde7992666491ed2e0984b23e424f964
harizMunawar <munawarhariz@gmail.com>
2021-06-07 18:41:53 +0700

    Jikanized the anime search cogs

--- cogs/wibu.py
+++ cogs/wibu.py
@@ -1,8 +1,7 @@
 from discord.ext import commands
 from discord import Embed
-from animec.anicore import Anime
-from animec.anicore import NotFound404
 import re
+from jikanpy import AioJikan
 
 def wibu_404(context: str):
     not_found_embed = Embed(color=0xff0000)
@@ -11,7 +10,7 @@ def wibu_404(context: str):
 
 def split_eps(song_list: list):
     if len(song_list) <= 1: return song_list
-    return [re.split(r"\(([A-Za-z0-9\s-]+){5,}", op)[0] for op in song_list]
+    return [re.split(r'\(([A-Za-z0-9\s-]+){5,}', op)[0] for op in song_list]
 
 class Wibu(commands.Cog):
 
@@ -20,33 +19,45 @@ class Wibu(commands.Cog):
 
     @commands.command(aliases=['a', 'kartun'])
     async def anime(self, ctx, *, query: str):
-        try:
-            async with ctx.typing():
-                anime = Anime(query)
-                title = anime.title_english + f" ({anime.title_jp})" if anime.title_jp and anime.title_english else anime.name
-                desc = anime.description if len(anime.description) < 1024 else anime.description[0:1021]+"..."
+        _jikan = AioJikan()
+        async with ctx.typing():
+            try:
+                search = await _jikan.search(search_type='anime', query=query)
+                anime = await _jikan.anime(id=search['results'][0]['mal_id'])
 
-                anime_embed = Embed(color=0x00f00, title=title, url=anime.url)
-                anime_embed.set_thumbnail(url=anime.poster)
+                title = anime['title_english'] + f" ({anime['title_japanese']})" if anime['title_japanese'] and anime['title_english'] else anime['title']
+                desc = anime['synopsis'] if len(anime['synopsis']) < 1024 else anime['synopsis'][0:1021]+'...'
+
+                anime_embed = Embed(color=0x00f00, title=title, url=anime['url'])
+                anime_embed.set_thumbnail(url=anime['image_url'])
                 anime_embed.add_field(name='Sinopsis', value=desc, inline=False)
-                anime_embed.add_field(name='Episode', value=anime.episodes)
-                anime_embed.add_field(name='Ranking', value=anime.ranked)
-                anime_embed.add_field(name='Aired', value=anime.aired)
+                anime_embed.add_field(name='Episode', value=anime['episodes'])
+                anime_embed.add_field(name='Score', value=anime['score'])
+                anime_embed.add_field(name='Ranking', value=anime['rank'])
+                anime_embed.add_field(name='Popularity', value=anime['popularity'])
+                anime_embed.add_field(name='Rating', value=anime['rating'])
+                anime_embed.add_field(name='Aired', value=anime['aired']['string'])
 
-                if anime.opening_themes:
-                    op_list = split_eps(anime.opening_themes)
+                if anime['genres']:
+                    genre_list = [genre['name'] for genre in anime['genres']]
+                    genre_inline = ', '.join(genre_list)
+                    anime_embed.add_field(name='Genre', value=genre_inline[0:1024], inline=False)
+
+                if anime['opening_themes']:
+                    op_list = split_eps([op for op in anime['opening_themes']])
                     op_inline = '\n'.join(op_list)
-                    anime_embed.add_field(name="Opening", value=op_inline[0:1024], inline=False)
+                    anime_embed.add_field(name='Opening', value=op_inline[0:1024], inline=False)
 
-                if anime.ending_themes:
-                    ed_list = split_eps(anime.ending_themes)
+                if anime['ending_themes']:
+                    ed_list = split_eps([ed for ed in anime['ending_themes']])
                     ed_inline = '\n'.join(ed_list)
-                    anime_embed.add_field(name="Ending", value=ed_inline[0:1024], inline=False)
+                    anime_embed.add_field(name='Ending', value=ed_inline[0:1024], inline=False)
 
-                await ctx.send(embed=anime_embed)
-        except NotFound404:
-            await ctx.send(embed=wibu_404('Anime'))
-            return
+                result = await ctx.send(embed=anime_embed)
+                result.add_reactions('❗')
+                await _jikan.close()
+            except:
+                await _jikan.close()
 
 def setup(bot):
     bot.add_cog(Wibu(bot))
