3fde68a0b6666dcafa61c1be9e377591c3f4475a
Hariz Sufyan Munawar <munawarhariz@gmail.com>
2021-08-07 06:01:58 +0000

    Fix reddit GUI

--- cogs/reddit.py
+++ cogs/reddit.py
@@ -4,6 +4,7 @@ from discord import Embed
 from prawcore import exceptions
 import base64
 from discord_message_components import Button
+import asyncio
 
 class RedditClient:
     id_utf8 = base64.b64decode('bDEyRlBLVndGa2RuQmc=')
@@ -31,9 +32,13 @@ def nsfw_check(search_query: str):
         return True
     return False
 
-def search_submission(reddit_client, subreddit_search, submission_search):
-    if submission_search != "":           
-        return random.choice([x for x in reddit_client.subreddit(subreddit_search).search(submission_search, limit=100)])
+def search_submission(reddit_client, subreddit_search, submission_search, sort, time_filter):
+    if submission_search != "":      
+        return random.choice([x for x in reddit_client.subreddit(subreddit_search).search(
+            query=submission_search,
+            sort=sort,
+            time_filter=time_filter,
+            limit=100)])
 
     return random.choice([x for x in reddit_client.subreddit(subreddit_search).hot(limit=100)])
 
@@ -46,14 +51,61 @@ def check_is_imgur(url):
 
   return url, url.__contains__("imgur.com")
 
+def sort_check(search):
+    valid_sort = [
+      'sort:new',
+      'sort:relevance',
+      'sort:hot',
+      'sort:top',
+      'sort:comments',
+    ]
+
+    if 'sort:' in search:
+        for sort in valid_sort:
+            if search.__contains__(str(sort)):
+                search = search.replace(str(sort), "")
+                return str(sort)[5:], search
+
+        search = search.replace('sort:', '')
+        return 'invalid', search
+
+    return 'relevance', search
+
+def time_check(search):
+    valid_time = [
+      'time:all',
+      'time:day',
+      'time:hour',
+      'time:month',
+      'time:week',
+      'time:year',
+    ]
+    if 'time:' in search:
+        for times in valid_time:
+            if search.__contains__(str(times)):
+                search = search.replace(times, "")
+                return times[5:], search
+
+        search = search.replace('time:', '')
+        return 'invalid', search
+    
+    return 'all', search
+
+from datetime import datetime
+
+def utc_to_local(utc):
+    return datetime.utcfromtimestamp(utc).strftime('%Y-%m-%d %H:%M:%S')
 
 class Reddit(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    async def get_post(subreddit_search, submission_search, reddit_client):
+    def get_post(self, subreddit_search, submission_search, reddit_client):
         nsfw = nsfw_check(submission_search)
-        posts = search_submission(reddit_client, subreddit_search, submission_search)
+        sort, submission_search = sort_check(submission_search)
+        time_filter, submission_search = time_check(submission_search)
+        posts = search_submission(reddit_client, subreddit_search, submission_search, sort, time_filter)
+        
         iteration = 0
         posts.url, imgur = check_is_imgur(posts.url)
 
@@ -66,10 +118,12 @@ class Reddit(commands.Cog):
         embedVar.set_author(name=f'Post by /u/{posts.author}', url=f'https://www.reddit.com/user/{posts.author}')
 
         if posts.link_flair_text:
-            embedVar.add_field(name='Flair', value=f"{posts.link_flair_text}", inline=False)
+            embedVar.add_field(name='Flair', value=f"{posts.link_flair_text}", inline=True)
 
         if posts.over_18:
-            embedVar.add_field(name='Marked', value="NSFW", inline=False)
+            embedVar.add_field(name='Marked', value="NSFW", inline=True)
+
+        embedVar.add_field(name='Posted On', value=utc_to_local(posts.created_utc))
         
         if posts.thumbnail and (check_is_valid_image(posts.url) or imgur):
             embedVar.set_image(url=posts.url)
@@ -90,21 +144,30 @@ class Reddit(commands.Cog):
             reddit = await ctx.channel.send(
                 embed = self.get_post(subreddit_search, submission_search, reddit_client),
                 components = [
-                    Button('reinvoke', 'Search Ini Lagi', 'green', ':mag_right:' )
+                    Button('reinvoke', 'Search Ini Lagi', 'green'),
+                    Button('delete', 'Delete', 'red')
                 ])
-            try:
-                while True:
-                    btn = await reddit.wait_for(self.bot, 'button', timeout=60)
-                    await btn.respond(ninja_mode=True)
+          
+        try:
+            while True:
+                btn = await reddit.wait_for(self.bot, 'button', timeout=60)
+                await btn.respond(ninja_mode=True)
 
-                    if btn.custom_id == 'reinvoke':
-                        reddit.edit(
-                            embed = self.get_post(subreddit_search, submission_search, reddit_client),
-                            components = [
-                                Button('reinvoke', 'Search Ini Lagi', 'green', ':mag_right:' )
-                            ])
-            except TimeoutError:
-                pass            
+                if btn.custom_id == 'reinvoke':
+                    await reddit.edit(
+                        embed = self.get_post(subreddit_search, submission_search, reddit_client),
+                        components = [
+                            Button('reinvoke', 'Search Ini Lagi', 'green'),
+                            Button('delete', 'Delete', 'red')
+                        ])
+                if btn.custom_id == 'delete':
+                    await reddit.delete()
+        except asyncio.TimeoutError:
+            await reddit.edit(
+                components = [
+                    Button('reinvoke', 'Search Ini Lagi', 'green', disabled=True),
+                    Button('delete', 'Delete', 'red', disabled=True)
+                ])
 
     @reddit.error
     async def reddit_error(self, ctx, error):
@@ -115,10 +178,14 @@ class Reddit(commands.Cog):
             await ctx.channel.send('Subreddit Gagal Ditemukan atau Tidak Tersedia')
 
         if isinstance(error, commands.CommandInvokeError):
-            await ctx.channel.send('Submission Gagal Ditemukan atau Tidak Tersedia')
+            if isinstance(error.original, exceptions.Redirect):
+                await ctx.channel.send('Subreddit Gagal Ditemukan atau Tidak Tersedia')
 
         if isinstance(error, commands.CommandOnCooldown): 
             await ctx.channel.send('Cooldown boss, coba lagi setelah {:.2f}s'.format(error.retry_after))
+
+        if isinstance(error, exceptions.ServerError):
+            pass
 
         raise error    
         
